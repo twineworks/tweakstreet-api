0661ebac563e9fc42750e69a2eb10c4261f98c71
Slawomir Chodnicki <slawomir.chodnicki@gmail.com>
2019-10-11 17:52:34 +0200

    adds generic value setting helper

---
+++ src/main/java/com/twineworks/tweakstreet/api/util/settings/ValueSetting.java
@@ -0,0 +1,58 @@
+package com.twineworks.tweakstreet.api.util.settings;
+
+import com.twineworks.tweakflow.lang.values.Value;
+import com.twineworks.tweakflow.lang.values.ValueProvider;
+
+import java.util.Map;
+
+public final class ValueSetting implements ValueProvider {
+
+  public final static class Builder {
+
+    private String name;
+    private ValueProvider valueProvider;
+
+    public Builder(String name){
+      this.name = name;
+    }
+
+    public Builder from(Map<String, ValueProvider> settings){
+      this.valueProvider = settings.get(name);
+      return this;
+    }
+
+    public ValueSetting build(){
+      return new ValueSetting(name, valueProvider);
+    }
+
+  }
+
+  private final ValueProvider valueProvider;
+  private final String name;
+
+  private Value value;
+
+  private ValueSetting(String name, ValueProvider valueProvider) {
+    this.valueProvider = valueProvider;
+    this.name = name;
+  }
+
+  public Value get(){
+
+    if (!hasChanged()) return value;
+
+    value = valueProvider.getValue();
+    return value;
+
+  }
+
+  public boolean hasChanged(){
+    return (valueProvider.getValue() != value);
+  }
+
+  @Override
+  public Value getValue() {
+    return valueProvider.getValue();
+  }
+
+}
