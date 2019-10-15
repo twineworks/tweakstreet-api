ec9b2717b4bbe0af19227f77987d9f7ba3916e6c
Slawomir Chodnicki <slawomir.chodnicki@gmail.com>
2019-10-15 18:00:11 +0200

    update file system abstraction

--- src/main/java/com/twineworks/tweakstreet/api/fs/FileSystemConnection.java
+++ src/main/java/com/twineworks/tweakstreet/api/fs/FileSystemConnection.java
@@ -1,18 +1,20 @@
 package com.twineworks.tweakstreet.api.fs;
 
+import com.twineworks.tweakstreet.api.fs.traversal.StorageMatcher;
+import com.twineworks.tweakstreet.api.fs.traversal.StorageVisitor;
+
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.charset.Charset;
 import java.nio.file.LinkOption;
 import java.util.List;
 
-import com.twineworks.tweakstreet.api.fs.traversal.StorageMatcher;
-import com.twineworks.tweakstreet.api.fs.traversal.StorageVisitor;
-
 public interface FileSystemConnection extends AutoCloseable {
-  InputStream newInputStream(String path) throws IOException;
-  BufferedWriter newBufferedWriter(String path, Charset charset, WriteOpenExistingOption ifExists) throws IOException;
+  InputStream newInputStream(String path, int bufferSize) throws IOException;
+  BufferedWriter newBufferedWriter(String path, Charset charset, int bufferSize, WriteOpenExistingOption ifExists) throws IOException;
+  OutputStream newOutputStream(String path, int bufferSize, WriteOpenExistingOption ifExists) throws IOException;
   boolean isRegularFile(String path, LinkOption... options) throws IOException;
   boolean isDirectory(String path, LinkOption... options) throws IOException;
   void walkFileTree(String path, int maxDepth, StorageVisitor v) throws IOException;
@@ -29,4 +31,5 @@ public interface FileSystemConnection extends AutoCloseable {
   void putContent(String path, InputStream is) throws IOException;
   void copy(String source, String target, boolean replace) throws IOException;
   void move(String src, String dst, boolean replace) throws IOException;
+  void close() throws IOException;
 }
