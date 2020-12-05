fca17a13be7084f36c857c7262c0e766095f650c
Slawomir Chodnicki <slawomir.chodnicki@gmail.com>
2020-12-05 10:23:44 +0100

    add ability to call functions to api

--- src/main/java/com/twineworks/tweakstreet/api/steps/context/StepContext.java
+++ src/main/java/com/twineworks/tweakstreet/api/steps/context/StepContext.java
@@ -1,9 +1,11 @@
 package com.twineworks.tweakstreet.api.steps.context;
 
+import com.twineworks.tweakflow.lang.interpreter.CallContext;
 import com.twineworks.tweakflow.lang.values.Value;
 import com.twineworks.tweakstreet.api.fs.FileSystemConnection;
 
 public interface StepContext {
   FileSystemConnection fileSystemConnection(Value def);
+  CallContext createCallContext();
   FlowInfo getFlowInfo();
 }
